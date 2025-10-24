55af1dab30ba398cd8ac2dc56f824009093a35a7
Mohit Kumar <mohit.kumar@testingxperts.com>
2025-10-24 17:17:40 +0530

    Add project files.

---
+++ SWGTeam/Neha.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Diagnostics;
+
+class Neha
+
+{
+
+    public static void CSharp()
+
+    {
+
+        //int NoOfProg = 9;
+
+        //string[] prog = new string[NoOfProg];
+
+        //prog[0] = "Read Write and Concatination Operations";
+
+        //prog[1] = "IF ELSE Example 1 (Number Negative OR Non Negative)";
+
+        //prog[2] = "IF ELSE Example 2 (Arithmetic Operators)";
+
+        //prog[3] = "If Else Example 3 (Greatest Among 3 Numbers)";
+
+        //prog[4] = "SWITCH & IF ELSE Example (ATM)";
+
+        //prog[5] = "SWITCH, DO WHILE Example (C # Cafe)";
+
+        //prog[6] = "FOR Loop Example 1 (Table using For Loop)";
+
+        //prog[7] = "List Example";
+
+        //Console.WriteLine("Select program to run:");
+
+        //Console.WriteLine(new string('-', 50));
+
+        //Console.WriteLine("{0,-5} | {1,-15}", "S.No.", "Excercise");
+
+        //Console.WriteLine(new string('-', 50));
+
+        //int m = 0;
+
+        //do
+
+        //{
+
+        //    Console.WriteLine("{0,-5} | {1,-15}", m + 1, prog[m]);
+
+        //    Console.WriteLine(new string('-', 50));
+
+        //    m++;
+
+        //} while (m < prog.Length);
+
+        //Console.WriteLine("Please Enter the Excercise S.No. to See the Result else 0 to Exit\a");
+
+        //string choice = Console.ReadLine() ?? "0";
+
+        //int update = 0;
+
+        //bool valid = int.TryParse(choice, out update);
+
+        bool valid = true;
+
+        int update = 8;
+
+        if (valid)
+
+        {
+
+            //int choice = Convert.ToInt32(Console.ReadLine());
+
+            //int choice = 8;
+
+            switch (update)
+
+            {
+
+                case 0:
+
+                    Console.WriteLine("Good Bye Happy Learning\a");
+
+                    return;
+
+                case 1:
+
+                    ReadWrite.Run();
+
+                    break;
+
+                case 2:
+
+                    Switch.Operation();
+
+                    break;
+
+                case 3:
+
+                    IfElse.Numerical();
+
+                    break;
+
+                case 4:
+
+                    Multiplicationwhile.Readingnumbers();
+
+                    break;
+
+                case 5:
+
+                    Breakcontinue.Run();
+
+                    break;
+
+                case 6:
+
+                    Dowhileloop.Run();
+
+                    break;
+
+                case 7:
+
+                    Billing.Run();
+
+                    break;
+
+                case 8:
+
+                    Multipication.Run();
+
+                    break;
+
+                    /*case 9:
+
+                       FruitBasket.Run();
+
+                       break;
+
+                  default:
+
+                      Console.WriteLine("Sorry Invalid choice.\a");*/
+
+
+            }
+
+        }
+
+        else
+
+        {
+
+            Console.WriteLine("Sorry Invalid Choice\a");
+
+            Main();
+
+        }
+
+    }
+
+}
+
---
+++ SWGTeam/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+class MainFile    
+{
+    public static void Main()
+    {
+        int NumbOfUser = 4;
+        string[] User = new string[NumbOfUser];
+
+        User[0] = "Sukhminder Kaur";
+        User[1] = "Neha Sharma";
+        User[2] = "Vikas Chandel";
+        User[3] = "Mohit Kumar";
+
+        Console.WriteLine("Swg Team");
+        Console.WriteLine(new string('-', 40));
+        Console.WriteLine("{0, -5} {1, -20}", "S.No", "| User");
+        Console.WriteLine(new string('-', 40));
+
+        int PrintUsers = 0;
+
+        foreach (string Users in User)
+        {
+
+            Console.WriteLine("{0, -5} {1, -20}", PrintUsers + 1, "| " + User[PrintUsers]);
+            Console.WriteLine(new string('-', 40));
+            PrintUsers++;
+        }
+        Console.WriteLine("Please Enter the Excercise S.No. to See the Result else 0 to Exit\a");
+        string choice = Console.ReadLine() ?? "0";
+        int update = 0;
+        bool valid = int.TryParse(choice, out update);
+        if (valid)
+        {
+            //int choice = Convert.ToInt32(Console.ReadLine());
+            //int choice = 8;
+            switch (update)
+            {
+                case 0:
+                    Console.WriteLine("Good Bye Happy Learning\a");
+                    return;
+                case 1:
+                    Sukhminder.Csharp();
+                    break;
+                case 2:
+                    Neha.CSharp();
+                    break;
+                case 3:
+                    Vikas.Csharp();
+                    break;
+                case 4:
+                    Mohit.CSharp();
+                    break;
+            }
+        }
+    }
+}
