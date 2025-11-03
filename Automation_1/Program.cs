221208ce0e9e0501d1c07b5115ff06f3617e6bf6
Mohit Kumar <mohit.kumar@testingxperts.com>
2025-11-03 21:42:57 +0530

    Sukhi project merge

---
+++ Automation_1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Sukhi_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+     AccessModifiers obj = new AccessModifiers();
+
+            Console.WriteLine("Accessing public variable 'year': " + obj.year);
+            Console.WriteLine(" Accessing internal variable 'month': " + obj.month);
+            obj.InternalMethod();
+            
+          
+        }
+    }
+}
